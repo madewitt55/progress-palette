b9df7d727e9704408f2f086e7db5351ae4019042
Mason DeWitt <madewitt55@gmail.com>
2025-01-27 16:03:34 -0500

    Setup electron app, vite react app, create scripts to launch

---
+++ dist-electron/util.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isDev = isDev;
+function isDev() {
+    return process.env.NODE_ENV == "development";
+}
+//# sourceMappingURL=util.js.map
